7ca154d6f4e0f3643e377996e4c92a1284321c67
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-03 10:16:21 +0300

    Нужно добавлять проверки на всё, иначе Exception

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      while (true){
+          System.out.println("Введите число: ");
+          String a = new Scanner(System.in).nextLine();
+          if(a.matches("\\d+") && a.length() < 10){
+              int newA = Integer.parseInt(a);
+              System.out.println(newA);
+          }
+          else {
+              System.out.println("это не число");
+          }
+      }
+    }
 }

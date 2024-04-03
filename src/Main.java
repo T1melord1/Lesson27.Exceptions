d13cd0e2aeb7cfa842546f65bddae9169de11edd
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-03 10:43:36 +0300

    Пример работы отлова Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      while (true){
-          System.out.println("Введите число: ");
-          String a = new Scanner(System.in).nextLine();
-          if(a.matches("\\d+") && a.length() < 10){
-              int newA = Integer.parseInt(a);
-              System.out.println(newA);
-          }
-          else {
-              System.out.println("это не число");
-          }
-      }
-    }
+        while (true) {
+            try {
+                System.out.println("Введите число: ");
+                int a = new Scanner(System.in).nextInt();
+                System.out.println(a);
+            } catch (InputMismatchException e){
+                System.out.println("Упс, ошибка!");
+            }catch (NullPointerException e){
+                System.out.println("Нул, о боже!");
+            }
+    }}
 }

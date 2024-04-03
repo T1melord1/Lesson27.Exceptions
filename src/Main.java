563b0421191c63da0225080999876089ec395dd1
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-03 11:05:04 +0300

    Пример работы отлова Exceptions через методы

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            try {
-                System.out.println("Введите число: ");
-                int a = new Scanner(System.in).nextInt();
-                System.out.println(a);
-            } catch (InputMismatchException e){
-                System.out.println("Упс, ошибка!");
-            }catch (NullPointerException e){
-                System.out.println("Нул, о боже!");
-            }
-    }}
+        try {
+            test1();
+            test2();
+
+            test1();
+            test2();
+        } catch (Exception e) {
+            System.out.println("Vvel huynu");
+        }
+
+
+    }
+    public static void test1() throws Exception {
+        System.out.println("Vvedi A");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println(a);
+    }
+    public static void test2() throws IOException{
+        System.out.println("Vvedi B");
+        int b = new Scanner(System.in).nextInt();
+        System.out.println(b);
+    }
 }
